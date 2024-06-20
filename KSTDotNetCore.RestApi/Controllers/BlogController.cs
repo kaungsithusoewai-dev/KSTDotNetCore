6012bd90774cfcdefea6d8011ba8239e6d4fbf3d
Kaung Si Thu <kimurashinji89kw@gmail.com>
2024-06-20 15:46:08 +0630

    [Complete] API EFCore CRUD

---
+++ KSTDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,114 @@
+﻿using KSTDotNetCore.RestApi.EFDbContext;
+using KSTDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KSTDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public BlogController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+        [HttpGet]
+        public IActionResult Read()
+        {
+            var lst = _appDbContext.Blogs.ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
+        {
+            var lst = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (lst == null)
+            {
+                return NotFound("No data found");
+            }
+            return Ok(lst);
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogModel blog)
+        {
+            _appDbContext.Blogs.Add(blog);
+            var result = _appDbContext.SaveChanges();
+            string message = result > 0 ? "Saving Successful" : "Saving Failed";
+            return Ok(message);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BlogModel blog)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item == null)
+            {
+                return NotFound("No data found");
+
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            var result = _appDbContext.SaveChanges();
+            string message = result > 0 ? "Updating Successful" : "Updating Failed";
+            return Ok(message);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, BlogModel blog)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound("No data found");
+
+            }
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            var result = _appDbContext.SaveChanges();
+            string message = result > 0 ? "Updating Successful" : "Updating Failed";
+            return Ok(message);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound("No data found");
+
+            }
+            _appDbContext.Blogs.Remove(item);
+            var result = _appDbContext.SaveChanges();
+            string message = result > 0 ? "Deleting Successful" : "Deleting Failed";
+            return Ok(message);
+        }
+    } 
+    }
+
+
+
+
---
+++ KSTDotNetCore.RestApi/EFDbContext/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using KSTDotNetCore.RestApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSTDotNetCore.RestApi.EFDbContext
+{
+    internal class AppDbContext : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
