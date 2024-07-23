57bf9bb1fc26268cff63abb36aa0810ebbc3f5dd
Kaung Si Thu <kimurashinji89kw@gmail.com>
2024-07-23 14:29:19 +0630

    [Complete] Blazor Web Aassembly

---
+++ KSTDotNetCore.BlazorWasmAppV2/Models/BlogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KSTDotNetCore.BlazorWasmAppV2.Models;
+public class BlogModel
+{
+ 
+    public string? BlogId { get; set; }
+
+    public string? BlogTitle { get; set; }
+
+    public string? BlogAuthor { get; set; }
+
+    public string? BlogContent { get; set; }
+}
+//public record BlogEntity(int BlogId, string BlogTitle, string BlogAuthor, string BlogContent);
